72cdba2a3e95a0acc0130f744dda639e63b31d28
kmarkert <kel.markert@gmail.com>
2018-04-27 15:35:48 -0500

    fixed bug with masked elevation bands in snow parameter generation

--- scripts/format_snow_params.py
+++ scripts/format_snow_params.py
@@ -6,18 +6,19 @@
 # MODIFIED BY: n/a
 # CREATION DATE: 28 Oct. 2016
 # LAST MOD DATE: 03 Apr. 2017
-# PURPOSE: This script takes geotiff data for snow band parameterization and converts
+# PURPOSEe This script takes geotiff data for snow band parameterization and converts
 #          it to the needed text format for the VIC model
 # DEPENDENCIES: numpy, osgeo (gdal)
 #******************************************************************************
 
 # import dependencies
+from __future__ import print_function
 import os
 import sys
 import warnings
 import numpy as np
 from osgeo import gdal
-from osgeo.gdalnumeric import *  
+from osgeo.gdalnumeric import *
 from osgeo.gdalconst import *
 
 # set system to ignore simple warnings
@@ -34,17 +35,17 @@ def format_snow_params(basinMask,elvHiRes,outSnow,interval):
     RETURNS: n/a
     NOTES: Does not return a variable but writes an output file
     """
-    
+
     band = 1 # constant variable for reading in data
 
     interval = int(interval) # force equal interval value to be int type
-    
+
     # make a list of input raster files
     infiles = [basinMask,elvHiRes]
-    
+
     # try to read in the raster data
     try:
-        
+
         # read basin grid raster
         ds = gdal.Open(infiles[0],GA_ReadOnly)
         b1 = ds.GetRasterBand(band)
@@ -52,152 +53,166 @@ def format_snow_params(basinMask,elvHiRes,outSnow,interval):
         maskRes = ds.GetGeoTransform()[1]
         ds = None
         b1 = None
-        
+
         # read hi res elevation raster
         ds = gdal.Open(infiles[1],GA_ReadOnly)
         b1 = ds.GetRasterBand(band)
         elvhires = BandReadAsArray(b1)
-        clsRes = ds.GetGeoTransform()[1] 
+        clsRes = ds.GetGeoTransform()[1]
         ds = None
         b1 = None
-    
+
     # if not working, give error message
     except AttributeError:
         raise IOError('Raster file input error, check that all paths are correct')
-        
+
     # mask elevation values less than 0
-    elvhires = np.ma.masked_where(elvhires<0,elvhires)
-    
+    elvhires[np.where(elvhires<0)] = np.nan
+
     # get ratio of high resoltion to low resolution
     clsRatio = int(maskRes/clsRes)
-    
+
     # check if the output parameter file exists, if so delete it
     if os.path.exists(outSnow)==True:
-        os.remove(outSnow)    
+        os.remove(outSnow)
 
     nbands = [] # blank list
-    
+
     # try to write to output snow parameter file
     try:
-    
+
         with open(outSnow, 'w') as f:
-            
+
             cnt = 1 # set grid cell id counter
-            
+
             # pass counter
             pass_counter = range(2)
-            
+
             # perform two passes on the raster data
             # 1) to grab the maximum number of bands for a given pixel
             # 2) to calculate the snow band parameters and write to output file
             for pass_cnt in pass_counter:
-            
+
                 # loop over each pixel in the template raster
                 for i in range(mask.shape[0]):
                     cy1 = i*clsRatio
                     cy2 = cy1+clsRatio
-        
-                    for j in range(mask.shape[1]):     
+
+                    for j in range(mask.shape[1]):
                         cx1 = j*clsRatio
                         cx2 = cx1+clsRatio
-        
+
                         # get all hi res pixels in a template pixel
                         tmp = elvhires[cy1:cy2,cx1:cx2]
-      		
-      		            # find min and max values for interval
-                        minelv = np.min(tmp.astype(int)) - (np.min(tmp.astype(int))%interval)
-                        maxelv = np.max(tmp.astype(int)) + (np.max(tmp.astype(int))%interval)
-                                    
-                        # create an array of band limits                                                 
-                        bands = np.arange(minelv, maxelv+interval,interval)
-      		    
+                        if tmp.size == 0:
+			    tmp=tmp2[:]
+      		        
+                        
                         # create blank array for number of bands calculation...
                         if mask[i,j] == 1: # ...if it is not a masked pixel
-        
+			    tmp2=tmp
+
+			    if np.all(tmp == np.nan) == True:
+				tmp[:,:] = 0
+				
+                            # find min and max values for interval
+                            minelv = np.nanmin(tmp.astype(int)) - (np.nanmin(tmp.astype(int))%interval)
+      		            maxelv = np.nanmax(tmp.astype(int)) + (np.nanmax(tmp.astype(int))%interval)
+                        
+                            #print(np.min(tmp).mask)
+                            # create an array of band limits
+      		            bands = np.arange(minelv, maxelv+interval,interval)
+
       		            bcls = np.zeros_like(tmp)
       		            bcls[:,:] = -1
-      		        
+
                             # get the number of bands per pixel
           		    for b in range(bands.size-1):
-         			
-                        bcls[np.where((tmp>=bands[b])&(tmp<bands[b+1]))] = b # band counter
-                                
+
+                                bcls[np.where((tmp>=bands[b])&(tmp<bands[b+1]))] = b # band counter
+
                                 # if it's the first pass get number of bands for each pixel
                                 if pass_cnt == 0:
                                     uniqcnt = np.unique(bcls[np.where(tmp>0)])
           		            nbands.append(uniqcnt.size) # save to a list for second pass
-          		            
+
           		    if pass_cnt == 1:
                                 uniqcnt = np.unique(bcls[np.where(tmp>0)])
-                                #clscnt = np.bincount(tmp.ravel())                
-                                
+                                #clscnt = np.bincount(tmp.ravel())
+
                                 f.write('{0}\t'.format(cnt)) # write grid cell id
-                                
+
                                 # find frational area for each band and write to file
                                 for c in range(maxbands):
                                     try:
                                         idx = np.where(bcls==uniqcnt[c])
-                                        frac = np.float(idx[0].size) / np.float(bcls[np.where(bcls>=0)].size)
+                                        num = np.float(bcls[np.where(bcls>=0)].size)
+					if num == 0:
+					    num = np.float(idx[0].size)
+                                        frac = np.float(idx[0].size) / num
                                     except IndexError:
                                         frac = 0
-                                        
+
              			    f.write('{0:.4f}\t'.format(frac))
-             			    
+
                                 # calculate the mean elevation for each band and write to file
                                 for c in range(maxbands):
                                     try:
                                         idx = np.where(bcls==uniqcnt[c])
                                         muelv = np.nanmean(tmp[idx])
-                                            
+
                                     except IndexError:
                                         muelv = 0
-                                            
+
                                     f.write('{0:.4f}\t'.format(muelv))
-                                    
+
                                 # calculate the precipitation fractions and write to file
       		                for c in range(maxbands):
      			            try:
                                         idx = np.where(bcls==uniqcnt[c])
-                                        frac = np.float(idx[0].size) / np.float(bcls[np.where(bcls>=0)].size)
+					num = np.float(bcls[np.where(bcls>=0)].size)
+					if num == 0:
+					    num = np.float(idx[0].size)
+                                        frac = np.float(idx[0].size) / num
                                     except IndexError:
                                         frac = 0
-                                        
+
      			            f.write('{0:.4f}\t'.format(frac))
-     			            
+
      			        f.write('\n') # write return value for new line
-          		
-       	                if pass_cnt == 1:
-                            cnt += 1 # plus one to the grid cell id counter 
-                            
+
+       	                if pass_cnt == 1 & mask[i,j] == 1:
+                            cnt += 1 # plus one to the grid cell id counter
+
                 if pass_cnt == 0:
-       	            maxbands = max(nbands) # maximum number of bands for a pixel  
-                         
+       	            maxbands = max(nbands) # maximum number of bands for a pixel
+
         # print the number of bands for user to input into global parameter file
-        print 'Number of maximum bands: {0}'.format(maxbands)  
-                    
-    # except raise an error when it doesn't work                
+        print('Number of maximum bands: {0}'.format(maxbands))
+
+    # except raise an error when it doesn't work
     except IOError:
         raise IOError('Cannot write output file, error with output snow parameter file path')
-                            
+
     return
-                
+
 def main():
     n_args = len(sys.argv)
-    
+
     # Check user inputs
     if n_args != 5:
-        print "Wrong user input"
-        print "Script writes the snow band parameter file for the VIC model"
-        print "usage: python format_snow_params.py <template raster> <elevation raster> <output snow band file> <interval for snow bands>"
-        print "Exiting system..."
+        print ("Wrong user input")
+        print ("Script writes the snow band parameter file for the VIC model")
+        print ("usage: python format_snow_params.py <template raster> <elevation raster> <output snow band file> <interval for snow bands>")
+        print ("Exiting system...")
         sys.exit()
-    
+
     else:
         # Pass command line arguments into function
         format_snow_params(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
-        
-    return  
+
+    return
 
 # Execute the main level program if run as standalone
 if __name__ == "__main__":
-    main() 
+    main()
